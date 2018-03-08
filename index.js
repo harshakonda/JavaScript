237f556a4ddce537e0bc8225aefd3527c485f3a3
HarsHa Konda <harsha.konda@yrcfreight.com>
2018-03-08 17:27:08 -0600

    intial work !!

---
+++ index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by uyr97d7 on 3/8/2018.
+ */
+
+
+var javaScriptBasics = {
+    'Variables': '',
+    'Comments': '',
+    'Operators':'',
+    'Conditionals':'',
+    'Functions': '',
+    'Events' : ''
+};
+
+console.log("Testing in console", javaScriptBasics);
